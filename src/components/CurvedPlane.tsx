85867dd013e10b019b6f194b842001090669669f
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-07-21 12:00:00 +0200

    rotation fix

--- src/components/CurvedPlane.tsx
+++ src/components/CurvedPlane.tsx
@@ -35,7 +35,7 @@ export default function CurvedPlane() {
     return () => {
       document.removeEventListener("mousemove", onMouseMove);
     };
-  }, []);
+  }, [windowWidth, windowHeight]);
 
   useFrame(() => {
     const sphereMesh = sphereRef.current;
