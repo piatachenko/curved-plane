2ec5438587500382457fdb09287f80788b4599b1
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-07-23 12:01:00 +0200

    add hover effect

--- src/components/CylinderWork.tsx
+++ src/components/CylinderWork.tsx
@@ -1,12 +1,17 @@
 import { Cylinder } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
-import { useRef } from "react";
+import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import cylinderFragmentShader from "~/shaders/cylinderFragmentShader";
 import cylinderVertexShader from "~/shaders/cylinderVertexShader";
 
 export default function CylinderWork() {
   const cylinderRef = useRef(null);
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
+  const [key, setKey] = useState("");
+
   const idTexture = useLoader(THREE.TextureLoader, "/b.jpeg");
 
   const uniforms = {
@@ -14,13 +19,47 @@ export default function CylinderWork() {
     u_texture1: { value: idTexture },
     u_dispFactor: { value: 1 },
     u_effectFactor: { value: 1 },
-    // u_mouse: { value: new THREE.Vector2(0, 0) },
-    // u_mouseIntensity: { value: 0.01 },
-    // u_mouseRange: { value: 20.0 },
-    // u_mousePos: {
-    //   value: new THREE.Vector2(window.innerWidth, window.innerHeight),
-    // },
+    u_mouse: { value: new THREE.Vector2(0, 0) },
+    u_mouseIntensity: { value: 0.01 },
+    u_mouseRange: { value: 20.0 },
+    u_mousePos: {
+      value: new THREE.Vector2(windowWidth, windowHeight),
+    },
   };
+
+  useEffect(() => {
+    function updateWindow() {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+
+      uniforms.u_mousePos.value.x = window.innerWidth;
+      uniforms.u_mousePos.value.y = window.innerHeight;
+    }
+
+    updateWindow();
+    window.addEventListener("resize", updateWindow);
+    return () => {
+      window.removeEventListener("resize", updateWindow);
+    };
+  }, []);
+
+  useEffect(() => {
+    function handlePointerMove(e: MouseEvent) {
+      setKey(`${uniforms.u_mousePos.value.x}${uniforms.u_mousePos.value.y}`);
+
+      const x = ((e.clientX - windowWidth / 2) / windowWidth) * 2;
+      const y = -((e.clientY - windowHeight / 2) / windowHeight) * 2;
+
+      gsap.to(uniforms.u_mouse.value, 1.25, {
+        x,
+        y,
+        ease: "power3",
+      });
+    }
+
+    window.addEventListener("mousemove", handlePointerMove);
+    return () => window.addEventListener("mousemove", handlePointerMove);
+  }, [cylinderRef, windowWidth, key]);
 
   return (
     <Cylinder
@@ -29,6 +68,7 @@ export default function CylinderWork() {
       ref={cylinderRef}
     >
       <shaderMaterial
+        key={key}
         vertexShader={cylinderVertexShader}
         fragmentShader={cylinderFragmentShader}
         side={THREE.BackSide}
