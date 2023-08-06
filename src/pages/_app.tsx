bba34c41092f206570f4a8f62377d3c07f0f3bfc
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-08-06 10:40:23 +0200

    add major mono font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { Major_Mono_Display } from "next/font/google";
 import "~/styles/globals.css";
 
+const majorMono = Major_Mono_Display({
+  weight: "400",
+  subsets: ["latin"], 
+})
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return <div className={majorMono.className}>
+    <Component {...pageProps} />
+  </div>;
 };
 
 export default MyApp;
